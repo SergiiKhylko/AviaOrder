d516f13975811e93b42f42e8e39be44398d82f90
SergiiKhylko <aj7000y@gmail.com>
2016-11-26 12:54:17 +0200

    added FlightDb

---
+++ src/main/java/com/ajkko/aviaorder/db/FlightDb.java
@@ -0,0 +1,114 @@
+package com.ajkko.aviaorder.db;
+
+import com.ajkko.aviaorder.objects.Flight;
+import com.ajkko.aviaorder.objects.spr.City;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+
+import static com.ajkko.aviaorder.utils.DbUtils.closeResultSet;
+import static com.ajkko.aviaorder.utils.DbUtils.closeStatement;
+import static com.ajkko.aviaorder.utils.DbUtils.getByIdStatement;
+
+public class FlightDb {
+
+    private static FlightDb instance;
+    private static final Logger LOG = LogManager.getLogger(FlightDb.class);
+    private static final String SQL_GET_FLIGHTS = "select * from flight";
+    private static final String SQL_GET_FLIGHT = "select * from flight where id = ?";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_CODE = "code";
+    private static final String COLUMN_DEPART = "date_depart";
+    private static final String COLUMN_DATE_COME = "date_come";
+    private static final String COLUMN_AIRCRAFT_ID = "aircraft_id";
+    private static final String COLUMN_CITY_FROM_ID = "city_from_id";
+    private static final String COLUMN_CITY_TO_ID = "city_to_id";
+
+    private FlightDb() {
+    }
+
+    public static FlightDb getInstance() {
+        if (instance == null) {
+            instance = new FlightDb();
+        }
+        return instance;
+    }
+
+    public Collection<Flight> getFlights(){
+        try {
+            return getFlights(MainDb.getInstance().getStatement());
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            MainDb.getInstance().closeConnection();
+        }
+        return new ArrayList<>();
+    }
+
+    private Collection<Flight> getFlights(Statement statement) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(SQL_GET_FLIGHTS);
+        Collection<Flight> flights = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                flights.add(getMappedFlight(resultSet));
+            }
+        } finally {
+            closeStatement(statement);
+            closeResultSet(resultSet);
+        }
+        return flights;
+    }
+
+    private Flight getMappedFlight(ResultSet resultSet) throws SQLException {
+        Flight flight = new Flight();
+        flight.setId(resultSet.getLong(COLUMN_ID));
+        flight.setCode(resultSet.getString(COLUMN_CODE));
+        flight.setDateCome(getDate(resultSet.getLong(COLUMN_DATE_COME)));
+        flight.setDateDepart(getDate(resultSet.getLong(COLUMN_DEPART)));
+        flight.setAircraft(AircraftDb.getInstance().get(resultSet.getLong(COLUMN_AIRCRAFT_ID)));
+        flight.setCityFrom(CityDb.getInstance().get(resultSet.getLong(COLUMN_CITY_FROM_ID)));
+        flight.setCityTo(CityDb.getInstance().get(resultSet.getLong(COLUMN_CITY_TO_ID)));
+        return flight;
+    }
+
+    private Calendar getDate(long date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(date);
+        return calendar;
+    }
+
+    public Flight getFlight(long id){
+        try {
+            return get(id);
+        } catch (SQLException e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            MainDb.getInstance().closeConnection();
+        }
+        return null;
+    }
+
+    protected Flight get(long id) throws SQLException {
+        return getFlight(getByIdStatement(SQL_GET_FLIGHT, id));
+    }
+
+    private Flight getFlight(PreparedStatement statement) throws SQLException {
+        ResultSet resultSet = null;
+        try {
+            resultSet = statement.executeQuery();
+            resultSet.next();
+            return getMappedFlight(resultSet);
+        } finally {
+            closeResultSet(resultSet);
+            closeStatement(statement);
+        }
+    }
+
+}
--- src/main/java/com/ajkko/aviaorder/objects/Flight.java
+++ src/main/java/com/ajkko/aviaorder/objects/Flight.java
@@ -8,10 +8,9 @@ import java.util.Calendar;
 public class Flight {
     private long id;
     private String code;
-    private Calendar flightDate;
-    private Calendar flightTime;
+    private Calendar dateDepart;
+    private Calendar dateCome;
     private Aircraft aircraft;
-    private long duration;
     private City cityFrom;
     private City cityTo;
 
@@ -31,36 +30,12 @@ public class Flight {
         this.code = code;
     }
 
-    public Calendar getFlightDate() {
-        return flightDate;
-    }
-
-    public void setFlightDate(Calendar flightDate) {
-        this.flightDate = flightDate;
-    }
-
-    public Calendar getFlightTime() {
-        return flightTime;
-    }
-
-    public void setFlightTime(Calendar flightTime) {
-        this.flightTime = flightTime;
-    }
-
     public Aircraft getAircraft() {
         return aircraft;
     }
 
     public void setAircraft(Aircraft aircraft) {
         this.aircraft = aircraft;
-    }
-
-    public long getDuration() {
-        return duration;
-    }
-
-    public void setDuration(long duration) {
-        this.duration = duration;
     }
 
     public City getCityFrom() {
@@ -79,6 +54,22 @@ public class Flight {
         this.cityTo = cityTo;
     }
 
+    public Calendar getDateDepart() {
+        return dateDepart;
+    }
+
+    public void setDateDepart(Calendar dateDepart) {
+        this.dateDepart = dateDepart;
+    }
+
+    public Calendar getDateCome() {
+        return dateCome;
+    }
+
+    public void setDateCome(Calendar dateCome) {
+        this.dateCome = dateCome;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -87,10 +78,9 @@ public class Flight {
         Flight flight = (Flight) o;
 
         if (id != flight.id) return false;
-        if (duration != flight.duration) return false;
         if (code != null ? !code.equals(flight.code) : flight.code != null) return false;
-        if (flightDate != null ? !flightDate.equals(flight.flightDate) : flight.flightDate != null) return false;
-        if (flightTime != null ? !flightTime.equals(flight.flightTime) : flight.flightTime != null) return false;
+        if (dateDepart != null ? !dateDepart.equals(flight.dateDepart) : flight.dateDepart != null) return false;
+        if (dateCome != null ? !dateCome.equals(flight.dateCome) : flight.dateCome != null) return false;
         if (aircraft != null ? !aircraft.equals(flight.aircraft) : flight.aircraft != null) return false;
         if (cityFrom != null ? !cityFrom.equals(flight.cityFrom) : flight.cityFrom != null) return false;
         return !(cityTo != null ? !cityTo.equals(flight.cityTo) : flight.cityTo != null);
@@ -101,12 +91,24 @@ public class Flight {
     public int hashCode() {
         int result = (int) (id ^ (id >>> 32));
         result = 31 * result + (code != null ? code.hashCode() : 0);
-        result = 31 * result + (flightDate != null ? flightDate.hashCode() : 0);
-        result = 31 * result + (flightTime != null ? flightTime.hashCode() : 0);
+        result = 31 * result + (dateDepart != null ? dateDepart.hashCode() : 0);
+        result = 31 * result + (dateCome != null ? dateCome.hashCode() : 0);
         result = 31 * result + (aircraft != null ? aircraft.hashCode() : 0);
-        result = 31 * result + (int) (duration ^ (duration >>> 32));
         result = 31 * result + (cityFrom != null ? cityFrom.hashCode() : 0);
         result = 31 * result + (cityTo != null ? cityTo.hashCode() : 0);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", dateDepart=" + dateDepart +
+                ", dateCome=" + dateCome +
+                ", aircraft=" + aircraft +
+                ", cityFrom=" + cityFrom +
+                ", cityTo=" + cityTo +
+                '}';
     }
 }
